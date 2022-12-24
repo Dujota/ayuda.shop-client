113b53f165ef3866979aa5e367b8c33c6cc8408a
Denis S Dujota <denisdujota@gmail.com>
2022-12-23 21:21:20 -0500

    add reg comp

---
+++ src/components/auth/Register.tsx
@@ -0,0 +1,26 @@
+import { useForm } from "react-hook-form";
+// TODO: Is this even something we need?
+
+export default function RegistrationForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Email"
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input type="undefined" placeholder="Password" {...register} />
+      <input type="undefined" placeholder="Confirm Password" {...register} />
+
+      <input type="submit" />
+    </form>
+  );
+}
