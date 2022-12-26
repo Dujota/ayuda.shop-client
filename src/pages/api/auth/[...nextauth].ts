ae08d6d2b9a6c8f1c21817c68dbd7f9ef09bc4e2
Denis S Dujota <denisdujota@gmail.com>
2022-12-26 15:20:07 -0500

    connect auth token from api to the next auth flow

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,17 +10,51 @@ import { env } from "../../../env/server.mjs";
 import type { UserAPI } from "@/types/auth.js";
 
 export const authOptions: NextAuthOptions = {
+  session: {
+    // Choose how you want to save the user session.
+    // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+    // If you use an `adapter` however, we default it to `"database"` instead.
+    // You can still force a JWT session by explicitly defining `"jwt"`.
+    // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+    // which is used to look up the session in the database.
+    // strategy: "database",
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 30 * 60, // 30 mins
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+
+    // The session token is usually either a random UUID or string, however if you
+    // need a more customized session token string, you can define your own generate function.
+    // generateSessionToken: () => {
+    //   return randomUUID?.() ?? randomBytes(32).toString("hex")
+    // }
+  },
+  jwt: {
+    // The maximum age of the NextAuth.js issued JWT in seconds.
+    // Defaults to `session.maxAge`.
+    maxAge: 60 * 30,
+    // You can define your own encode/decode functions for signing and encryption
+    // async encode() { },
+    // async decode() { },
+  },
   // Include user.id on session
   callbacks: {
     async session({ session, user, token }) {
       // TODO: Pull some information from our use from the rails backend
 
       // Add the rails api token to our session context
+      debugger;
       return { ...session, accessToken: token.accessToken };
     },
     // CONFIGURE BELOW FOR API CONNECTION
     async signIn({ user, account, profile, email, credentials }) {
       // TODO: Implement user role restrictions here
+      debugger;
       return true;
     },
     async redirect({ url, baseUrl }) {
@@ -30,7 +64,7 @@ export const authOptions: NextAuthOptions = {
       // copy over the rails token to the jwt payload
       if (user) {
         const data: UserAPI = { ...user };
-
+        debugger;
         if (data?.token) {
           return { ...token, accessToken: data.token };
         }
