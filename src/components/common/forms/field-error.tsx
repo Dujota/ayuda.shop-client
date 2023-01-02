af7062cc3b9c34f0638b81d572cba01b0afc7fb0
Denis S Dujota <denisdujota@gmail.com>
2023-01-02 10:34:55 -0500

    todo for future expansion of field error

--- src/components/common/forms/field-error.tsx
+++ src/components/common/forms/field-error.tsx
@@ -1,3 +1,35 @@
+// TODO: make it handle also generic messages
+/**
+ *
+ * React Hook Form field error ui
+ * https://react-hook-form.com/api/useform/seterror
+ *
+ *
+ * multiple api errors:
+ *
+ * {
+          [
+            {
+              type: "manual",
+              name: "username",
+              message: "Double Check This"
+            },{
+              type: "manual",
+              name: "firstName",
+              message: "Triple Check This"
+            }
+          ].forEach(({ name, type, message }) =>
+            setError(name, { type, message })
+ *
+ * single field erros:  setError("lastName", {
+      types: {
+        required: "This is required",
+        minLength: "This is minLength"
+      }
+    });
+ *
+ */
+
 const FieldError = ({ error }: any) => {
   if (!error) return null;
 
@@ -5,6 +37,7 @@ const FieldError = ({ error }: any) => {
     return <p>{error.message}</p>;
   }
 
+  // assumes a base string error
   return <p>{error}</p>;
 };
 
